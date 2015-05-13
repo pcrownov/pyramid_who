50639e022b1a78b3a986ee94cb1a57125b56ba36
Patrick Crownover <pcrownov@redhat.com>
2015-05-13 10:32:34 -0400

    logging

--- pyramid_who/whov2.py
+++ pyramid_who/whov2.py
@@ -31,7 +31,7 @@ class WhoV2AuthenticationPolicy(object):
     implements(IAuthenticationPolicy)
 
     def __init__(self, config_file, identifier_id, callback=_null_callback):
-        log.debug("PYRAMID_WHO_WHOV2 INIT START")
+        log.debug('__init__ : START')
         config_file = self._config_file = os.path.abspath(
                                           os.path.normpath(
                                           os.path.expandvars(
@@ -45,15 +45,19 @@ class WhoV2AuthenticationPolicy(object):
         self._callback = callback
 
     def unauthenticated_userid(self, request):
+        log.debug('unauthenticated_userid : START')
         identity = self._get_identity(request)
+        log.debug('unauthenticated_userid : %s' % identity)
         if identity is not None:
             return identity['repoze.who.userid']
 
     def authenticated_userid(self, request):
         """ See IAuthenticationPolicy.
         """
+        log.debug('authenticated_userid : START')
         identity = self._get_identity(request)
 
+        log.debug('authenticated_userid : %s' % identity)
         if identity is not None:
             groups = self._callback(identity, request)
             if groups is not None:
@@ -62,8 +66,11 @@ class WhoV2AuthenticationPolicy(object):
     def effective_principals(self, request):
         """ See IAuthenticationPolicy.
         """
+        log.debug('effective_principals : START')
         identity = self._get_identity(request)
+        log.debug('effective_principals : Identity : %s' % identity)
         groups = self._get_groups(identity, request)
+        log.debug('effective_principals : Groups : %s' % groups)
         if len(groups) > 1:
             groups.insert(0, identity['repoze.who.userid'])
         return groups
@@ -71,32 +78,38 @@ class WhoV2AuthenticationPolicy(object):
     def remember(self, request, principal, **kw):
         """ See IAuthenticationPolicy.
         """
-        log.debug("PCROWNOV - REMEMBER START")
+        log.debug('remember : START')
         api = self._getAPI(request)
         identity = {'repoze.who.userid': principal,
                     'identifier': api.name_registry[self._identifier_id],
                    }
+        log.debug('remember : Identity : %s' % identity)
         return api.remember(identity)
 
     def forget(self, request):
         """ See IAuthenticationPolicy.
         """
+        log.debug('forget : START')
         api = self._getAPI(request)
         identity = self._get_identity(request)
+        log.debug('forget : Identity : %s' % identity)
         return api.forget(identity)
 
     def _getAPI(self, request):
+        log.debug('_getAPI : START')
         return self._api_factory(request.environ)
 
     def _get_identity(self, request):
-        log.debug("GET IDENTITY START")
+        log.debug("_get_identity : START")
         identity = request.environ.get('repoze.who.identity')
+        log.debug("_get_identity : Identity : %s" % identity)
         if identity is None:
             api = self._getAPI(request)
             identity = api.authenticate()
         return identity
 
     def _get_groups(self, identity, request):
+        log.debug("_get_groups : START")
         if identity is not None:
             dynamic = self._callback(identity, request)
             if dynamic is not None:
